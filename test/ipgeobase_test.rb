f4f7b0666139357bb2ff15bbc2b6a043693fab28
Oleg Kholyk <oleg@kholyk.ru>
2022-02-06 17:37:54 +0300

    base functionality

--- test/ipgeobase_test.rb
+++ test/ipgeobase_test.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "ipgeobase"
 
-class IpgeobaseTest < Minitest::Test
+class IpgeobaseTest < Minitest::Test 
+  def setup
+    @geobase_object = Ipgeobase.lookup('188.243.183.134')
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Ipgeobase::VERSION
   end
 
   def test_it_does_something_useful
     assert true
+  end
+
+  def test_include
+    # Ipgeobase.class == 'Module'
+    assert @geobase_object.city == 'St Petersburg'
   end
 end
